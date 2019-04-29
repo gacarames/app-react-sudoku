8e1d2d468f042ce09a4bb5728496fa6588384710
Gomez Cuevas <tgomez@thxmedios.local>
2019-04-29 15:25:54 -0300

    cambios en estilos de Sudoku

--- src/CurrentDif.js
+++ src/CurrentDif.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
-import './css/current-dif.css';
+import React, { Component } from "react";
+import "./css/current-dif.css";
 
 export default class CurrentDif extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      level: "Facil"
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            level: 'Facil'
-        }
-    }
+  componentDidMount() {
+    this.setState({
+      level: document.querySelector(".active").innerHTML
+    });
+  }
 
-    componentDidMount() {
-        this.setState({
-            level: document.querySelector('.active').innerHTML
-        });
-    }
+  componentWillUpdate() {
+    this.setState({
+      level: document.querySelector(".active").innerHTML
+    });
+  }
 
-    componentWillUpdate() {
-        this.setState({
-            level: document.querySelector('.active').innerHTML
-        });
-    }
-
-    render() {
-
-        return (
-
-            <div className="sdk-current-dif-wrapper">
-                <span className="sdk-difficulty">{`Dificultad: ${this.state.level}`}</span>
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div className="sdk-current-dif-wrapper mb-2">
+        <span className="sdk-difficulty">{`Dificultad: ${
+          this.state.level
+        }`}</span>
+      </div>
+    );
+  }
 }
